f0d77e647afe59433050030083ca398163b75e3c
Valeriy Borodenko <valeriyy999@gmail.com>
2024-08-31 23:24:14 +0300

    add

--- src/scripts/generateContacts.js
+++ src/scripts/generateContacts.js
@@ -1,5 +1,27 @@
-const generateContacts = async (number) => {};
 
-generateContacts(5);
+import {createFakeContact} from '../utils/createFakeContact'; // Припускаємо, що createFakeContact знаходиться в тому ж директорії
 
-import fs from 'fs';
+async function generateContacts(numContacts) {
+    try {
+        // Читаємо існуючі дані з файлу
+        const data = await fs.readFile('src/db/db.json', 'utf8');
+        const existingContacts = JSON.parse(data);
+
+        // Створюємо нові контакти
+        const newContacts = [];
+        for (let i = 0; i < numContacts; i++) {
+            newContacts.push(createFakeContact());
+        }
+
+        // Об'єднуємо існуючі та нові контакти
+        const allContacts = [...existingContacts, ...newContacts];
+
+        // Записуємо оновлені дані у файл
+        await fs.writeFile('src/db/db.json', JSON.stringify(allContacts, null, 2));
+        console.log(`Додано ${numContacts} нових контактів`);
+    } catch (error) {
+        console.error('Помилка при генерації та збереженні контактів:', error);
+    }
+}
+
+export default generateContacts;
--- src/utils/createFakeContact.js
+++ src/utils/createFakeContact.js
@@ -1,5 +1,7 @@
-import { faker } from "@faker-js/faker";
+import fs from 'fs';
+import { faker } from '@faker-js/faker';
 
+// Константа для создания фейкового контакта
 export const createFakeContact = () => ({
   id: faker.string.uuid(),
   name: faker.person.fullName(),
@@ -7,4 +9,53 @@ export const createFakeContact = () => ({
   email: faker.internet.email(),
   job: faker.person.jobTitle(),
 });
+
+// Функция для записи контактов в файл
+async function writeContacts(data, filePath = 'src/db/db.json') {
+  if (!Array.isArray(data)) {
+    throw new Error('Переданные данные должны быть массивом');
+  }
+
+  try {
+    // Проверка существования файла и создание, если его нет
+    await fs.promises.access(filePath, fs.constants.F_OK);
+    console.log('Файл уже существует. Будет перезаписан.');
+  } catch (error) {
+    if (error.code === 'ENOENT') {
+      console.log('Файл не найден. Создается новый.');
+    } else {
+      console.error('Произошла ошибка при проверке файла:', error);
+    }
+  }
+
+  try {
+    const json = JSON.stringify(data, null, 2);
+    await fs.promises.writeFile(filePath, json);
+    console.log('Контакты успешно записаны в файл');
+  } catch (error) {
+    console.error('Ошибка при записи контактов:', error);
+  }
+}
+
+// Функция для чтения контактов из файла или генерации фейковых
+async function readContacts(filePath = 'src/db/db.json') {
+  try {
+    const data = await fs.promises.readFile(filePath, 'utf8');
+    return JSON.parse(data);
+  } catch (error) {
+    if (error instanceof SyntaxError) {
+      console.error('Ошибка при парсинге JSON:', error);
+      // Можно попробовать восстановить данные или сгенерировать новые
+    } else {
+      console.error('Ошибка при чтении файла:', error);
+    }
+
+    
+  }
+}
+
+export default { writeContacts, readContacts };
+
+
+
 
