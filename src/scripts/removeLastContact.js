75d485fd42e766c79e69fefb2556d1838800bb00
Valeriy Borodenko <valeriyy999@gmail.com>
2024-09-02 21:10:38 +0300

    add

--- src/scripts/removeLastContact.js
+++ src/scripts/removeLastContact.js
@@ -1,1 +1,15 @@
-
+import {
+    readContactsFromFile,
+    writeContactsToFile,
+  } from '../utils/fileOperations.js';
+  
+  export const removeLastContact = async () => {
+    const existingContacts = await readContactsFromFile();
+    if (existingContacts.length !== 0) {
+      existingContacts.pop();
+    }
+    await writeContactsToFile(existingContacts);
+  };
+  
+  removeLastContact();
+  
